79711f1e2783e209a0c6daf8367700e6bafe3a94
Nikolay Vitkov <nvitkov@chromium.org>
2025-10-01 13:42:44 +0200

    chore: misc fixes

--- src/logger.ts
+++ src/logger.ts
@@ -5,6 +5,7 @@
  */
 import fs from 'node:fs';
 
+import type {JSONRPCNotification} from '@modelcontextprotocol/sdk/types.js';
 import debug from 'debug';
 
 const mcpDebugNamespace = 'mcp:log';
@@ -18,12 +19,20 @@ export function saveLogsToFile(fileName: string): fs.WriteStream {
   // Enable overrides everything so we need to add them
   debug.enable(namespacesToEnable.join(','));
 
-  const logFile = fs.createWriteStream(fileName, {flags: 'a'});
+  const logFile = fs.createWriteStream(fileName, {flags: 'a+'});
   debug.log = function (...chunks: any[]) {
     logFile.write(`${chunks.join(' ')}\n`);
   };
   logFile.on('error', function (error) {
-    console.log(`Error when opening/writing to log file: ${error.message}`);
+    console.log(
+      JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'error/logging-file-creation',
+        params: {
+          error: `Error when opening/writing to log file: ${error.message}`,
+        },
+      } satisfies JSONRPCNotification),
+    );
     logFile.end();
     process.exit(1);
   });
