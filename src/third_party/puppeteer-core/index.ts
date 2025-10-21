131a3625eaed864cd18fefe84c07d34ad1f90e09
Piotr Paulski <piotrpaulski@chromium.org>
2025-10-21 10:00:42 +0000

    fix: missed type import and wrong deps type in puppeteer bundling

--- src/third_party/puppeteer-core/index.ts
+++ src/third_party/puppeteer-core/index.ts
@@ -7,3 +7,4 @@
 export {Locator, PredefinedNetworkConditions} from 'puppeteer-core';
 export {default as puppeteer} from 'puppeteer-core';
 export type * from 'puppeteer-core';
+export type {CdpPage} from 'puppeteer-core/internal/cdp/Page.js';
