c37f2a47557de62862ec00bb2406b1a62dbccdfe
Alina Varkki <alinavarkki@google.com>
2026-02-04 16:12:15 +0100

    fix build

--- src/main.ts
+++ src/main.ts
@@ -60,7 +60,7 @@ const server = new McpServer(
     title: 'Chrome DevTools MCP server',
     version: VERSION,
   },
-  {capabilities: {logging: {}}},
+  {capabilities: {logging: {}, resources: {}, tools: {}}},
 );
 server.server.setRequestHandler(SetLevelRequestSchema, () => {
   return {};
