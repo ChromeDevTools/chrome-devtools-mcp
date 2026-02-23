b24c2677f3439da4882c1f440eedaae738434280
Alex Rudenko <alexrudenko@chromium.org>
2026-02-23 11:54:25 +0100

    chore: fix format

--- scripts/eval_scenarios/isolated_context_test.ts
+++ scripts/eval_scenarios/isolated_context_test.ts
@@ -19,10 +19,13 @@ export const scenario: TestScenario = {
     `,
   },
   expectations: calls => {
-    console.log(JSON.stringify(calls, null, 2))
+    console.log(JSON.stringify(calls, null, 2));
     assert.strictEqual(calls.length, 2);
     assert.ok(calls[0].name === 'new_page', 'First call should be navigation');
-    assert.deepStrictEqual(calls[0].args.isolatedContext, "contextB");
-    assert.ok(calls[1].name === 'take_screenshot', 'Second call should be a screenshot');
+    assert.deepStrictEqual(calls[0].args.isolatedContext, 'contextB');
+    assert.ok(
+      calls[1].name === 'take_screenshot',
+      'Second call should be a screenshot',
+    );
   },
 };
