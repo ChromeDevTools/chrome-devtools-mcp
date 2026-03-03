086f5a7a270d1489a1ab3c93e65c0f9042c32012
Nicholas Roscino <nroscino@google.com>
2026-03-03 16:50:24 +0100

    chore: rename env variable

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -92,7 +92,7 @@ async function runTests(attempt) {
 }
 
 const chromePath = installChrome('146.0.7680.31');
-process.env.TRIGGER_ACTION_EXECUTABLE_PATH = chromePath;
+process.env.CHROME_M146_EXECUTABLE_PATH = chromePath;
 
 const maxAttempts = shouldRetry ? 3 : 1;
 let exitCode = 1;
