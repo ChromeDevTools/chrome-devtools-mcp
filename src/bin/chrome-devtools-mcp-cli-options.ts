6fb8295c1bf5ceecce3f50ce8f227ed1fe818a0f
Nicholas Roscino <nroscino@google.com>
2026-03-18 16:57:17 +0100

    fix: removing default to false for categoryExtensions to fix tests

--- src/bin/chrome-devtools-mcp-cli-options.ts
+++ src/bin/chrome-devtools-mcp-cli-options.ts
@@ -211,7 +211,6 @@ export const cliOptions = {
   },
   categoryExtensions: {
     type: 'boolean',
-    default: false,
     hidden: true,
     conflicts: ['browserUrl', 'autoConnect', 'wsEndpoint'],
     describe:
